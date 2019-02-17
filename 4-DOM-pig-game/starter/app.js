7951253c7cdff46f30599fa784b80bc0154d16ff
oz izhak <oz@web-pick.com>
2019-02-17 16:09:08 +0200

    finish challenges in pig game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,11 +8,13 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, scoreInput;
+
 init();
 
 
-document.querySelector('.dice').style.display = 'none';
+document.getElementById('dice-1').style.display = 'none';
+document.getElementById('dice-2').style.display = 'none';
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
@@ -22,17 +24,24 @@ document.getElementById('current-1').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gamePlaying) {
         // 1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
         // 2.Display the result
-        var diceDOM = document.querySelector('.dice')
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
-
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-'+dice1+'.png';
+        document.getElementById('dice-2').src = 'dice-'+dice2+'.png';
+        // diceDOM.src = 'dice-' + dice + '.png';
+        if (dice1 === 6 && dice2 === 6) {
+            scores[activePlayer] = 0;
+            document.querySelector('#score-'+ activePlayer).textContent = '0';
+            return nextPlayer()
+        }
         // 3.Update the round score IF the rolled number was NOT a 1
-        if (dice !== 1) {
+        else if (dice1 !== 1 && dice2 !== 1) {
             // ADD SCORE
-            roundScore += dice;
+            roundScore += dice1 + dice2;
             document.getElementById('current-' + activePlayer).textContent = roundScore;
 
         } else {
@@ -47,12 +56,20 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     if (gamePlaying) {
         // add CURRENT score to GLOBAL score
         scores[activePlayer] += roundScore
+
+        var final_score = document.getElementById('scoreInput').value;
+        var winninngScore;
+        
+        winninngScore = final_score ? final_score : 100;
+
         // update the UI
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]
         // Check if player won the game
-        if (scores[activePlayer] >= 20) {
+        debugger;
+        if (scores[activePlayer] >= winninngScore) {
             document.getElementById('name-' + activePlayer).textContent = 'Winner!';
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
@@ -67,21 +84,25 @@ document.querySelector('.btn-new').addEventListener('click', init)
 
 
 function nextPlayer() {
+    lastDice = undefined;
     roundScore = 0;
     document.getElementById('current-' + activePlayer).textContent = 0;
     document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
     activePlayer = activePlayer === 0 ? 1 : 0;
     document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 }
 
 function init() {
-    scores = [0, 0];
+    scores       = [0, 0];
     activePlayer = 0;
-    roundScore = 0;
-    gamePlaying = true;
+    roundScore   = 0;
+    gamePlaying  = true;
+    pickSixArray = 0;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
